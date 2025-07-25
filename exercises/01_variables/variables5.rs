932c562ced5a858d75d98ac5ba83ac367da5cf2e
rishav76dev <rjrishav037@gmail.com>
2025-07-25 15:33:31 +0000

    done upto function

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -2,7 +2,7 @@ fn main() {
     let number = "T-H-R-E-E"; // Don't change this line
     println!("Spell a number: {}", number);
 
-    // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    // Shadow the variable with a new value of type i32
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
 }
